ae78cef517558d2ca4504d2b73bd809237938e02
esraanabeh <esraanabeh92@gmail.com>
2022-07-13 15:32:47 +0200

    logo
    logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <img src="{{ asset('vendors/dist/img/AdminLTELogo.png') }}" alt="Admin Logo"
+        <img src="{{ asset('upload/images/R.jpg') }}" alt="Admin Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Library
 </span>
