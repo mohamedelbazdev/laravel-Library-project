e2cddcd8e3b766f112b54c46e61db0af9e7c76fd
esraanabeh <esraanabeh92@gmail.com>
2022-07-14 15:13:10 +0200

    edit table

--- database/migrations/2022_07_12_155334_create_books_table.php
+++ database/migrations/2022_07_12_155334_create_books_table.php
@@ -14,7 +14,7 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('image');
             $table->text('desc');
