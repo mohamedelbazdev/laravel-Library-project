a21b9199826cd23c48ef26e1cbe967b93d81af3f
aya3omira <104975497+aya3omira@users.noreply.github.com>
2022-07-15 14:13:48 +0200

    update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Session;
+
 use App\Models\User;
-use Hash;
+use Illuminate\Support\Facades\Session;
+
+
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
